e66dec952ed99ece468b94b191c49e40ae0f2d53
mecies <maciekhnat@gmail.com>
2020-08-22 11:31:44 +0200

    Add posts and post by id resolver

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,23 @@
-import {} from "module";
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  // field is exposing to graphql schema
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
---
+++ src/resolvers/post.ts
@@ -0,0 +1,19 @@
+import { Query, Resolver, Ctx, Arg, Int } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+}
